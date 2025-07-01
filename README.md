fda78d3ce3cd42fece95725248c1f9afe5f8aa37
Mayur Prasanna <mayurpra@usc.edu>
2025-07-01 01:37:18 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Waste Classification using Transfer Learning
+
+TBD
+
---
+++ pages/overview.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Waste Classification Project Overview")
+st.write("Welcome to the project showcase. Use the sidebar to navigate.")
